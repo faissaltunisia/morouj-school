4f686c08c763dbc0576049f99597c13a60285f7b
SalalahSchool <aribifaissal@gmail.com>
2025-12-24 23:38:38 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'murooj-v6';
+const CACHE_NAME = 'murooj-v7';
 const assets = [
   './', './index.html', './style.css', './script.js', './splash.html', './manifest.json',
   'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2',
